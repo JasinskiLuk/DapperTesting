2dd88f94c5e4cbc1a6afe65162891397d8d6fa6d
Izantrop <46635757+Izantrop@users.noreply.github.com>
2020-03-28 21:57:56 +0100

    add function for add attachment to db and file to dir

--- DapperTesting.WebApp/Controllers/AttachmentController.cs
+++ DapperTesting.WebApp/Controllers/AttachmentController.cs
@@ -2,23 +2,39 @@
 using DapperTesting.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DapperTesting.WebApp.Controllers
 {
     public class AttachmentController : Controller
     {
         private readonly IAttachment _attachment;
-        public AttachmentController(IAttachment attachment)
+        private readonly IConfiguration _configuration;
+        public AttachmentController(IAttachment attachment, IConfiguration configuration)
         {
             _attachment = attachment;
+            _configuration = configuration;
         }
         public IActionResult Index()
         {
             return View();
         }
 
-        public async Task<IActionResult> AddAttachment(AttachmentDTO attachment, IFormFile files)
+        public async Task<IActionResult> AddFile(IFormFile files)
+        {
+            string path = _configuration.GetConnectionString("Path")+files.FileName;
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await files.CopyToAsync(stream);
+            }
+            return Ok();
+        }
+
+        public async Task<IActionResult> AddAttachment(AttachmentDTO attachment)
         {
             var i = await _attachment.AddAttachment(attachment);
             return Ok(i);
