6d6d7f7be1d99fe7155d72954daa3029d8c352a5
Izantrop <46635757+Izantrop@users.noreply.github.com>
2020-04-01 14:25:57 +0200

    add sqlmapper for datetime

--- DapperTesting.DbServices/DbDateTestService.cs
+++ DapperTesting.DbServices/DbDateTestService.cs
@@ -1,7 +1,8 @@
-﻿using Dapper;
+using Dapper;
 using DapperTesting.DTOs;
 using DapperTesting.Interfaces;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
@@ -17,6 +18,7 @@ namespace DapperTesting.DbServices
         public async Task AddDate(DateTestDTO DTO)
         {
             using SqlConnection conn = new SqlConnection(_connectionString);
+            SqlMapper.AddTypeMap(typeof(DateTime), DbType.DateTime2);
             await conn.QueryAsync(@"INSERT INTO [dbo].[DateTest]([Date1], [DateTime1], [DateTime2])
                                     VALUES(@Date1, @DateTime1, @DateTime2)",
                                         new { DTO.Date1, DTO.DateTime1, DTO.DateTime2 });
